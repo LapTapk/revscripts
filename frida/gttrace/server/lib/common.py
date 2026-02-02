e03d3c3f4911b6d1d79d33bba876f96454f13900
LapTapk <fret4657@gmail.com>
2026-02-03 00:01:04 +0300

    Remove some junk

--- frida/gttrace/server/lib/common.py
+++
@@ -1,9 +0,0 @@
-"""Shared value objects for gttrace."""
-
-from dataclasses import dataclass
-
-@dataclass(frozen=True)
-class ModRVA:
-    """Identify an address by module name and relative virtual address (RVA)."""
-    mod: str
-    rva: int
